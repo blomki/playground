52b06e47d9e768e37726492c2fabaafe4208e090
Julien FOUCHER <jfoucher@adws.amiltone.com>
2019-09-06 12:19:26 +0200

    création du formulaire

--- form.js
+++ form.js
@@ -1,19 +1,33 @@
 const page = document.querySelector(".layout-content");
+let userInputs = [];
+let userOutputs = [];
+
+const updateInfo = (value, index) => {
+  console.log(value, index);
+  // let output = event.target.value;
+  userOutputs[index].textContent = value;
+  if (userOutputs[2].textContent.length > 0) {
+    userOutputs[2].setAttribute("class", "email-placeholder");
+  }
+};
 
 const initForm = () => {
-  const page = document.querySelector(".layout-content");
   const template = document.querySelector("#form-template");
   const clone = document.importNode(template.content, true);
   page.setAttribute("id", "form");
   page.innerHTML = "";
   page.appendChild(clone);
-  //   const userInput = document.querySelector(".todo-input");
-  //   userInput.addEventListener("keyup", event => {
-  //     if (event.keyCode === 13) {
-  //       console.log("coucou");
-  //       addTodo(userInput.value);
-  //     }
-  //   });
+  const form = document.querySelector(".form");
+  console.log(form.clientHeight);
+  const preview = document.querySelector(".preview-container");
+  preview.style.height = `${form.clientHeight}px`;
+
+  userOutputs = Array.from(document.querySelectorAll(".output"));
+  userInputs = Array.from(document.querySelectorAll("input"));
+  console.log(userInputs, userOutputs);
+  userInputs.map((input, index) => {
+    input.addEventListener("keyup", updateInfo(event.target.value, index));
+  });
 };
 
 export { initForm };
